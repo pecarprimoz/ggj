5c029add763492abb7a5a45b9c46d5d2754207b2
dmajster <domen.rostohar98@gmail.com>
2020-02-01 16:37:11 +0100

    added multiple portals support

--- Assets/Resources/Portal/RecursivePortalCamera.cs
+++ Assets/Resources/Portal/RecursivePortalCamera.cs
@@ -5,54 +5,63 @@ namespace Assets.Resources.Portal
     public class RecursivePortalCamera : MonoBehaviour
     {
         [SerializeField]
-        private Portal[] portals = new Portal[2];
+        private Portal[] portals = new Portal[4];
+
+        [SerializeField]
+        private RenderTexture[] renderTextures;
 
         [SerializeField]
         private Camera portalCamera;
 
-        private RenderTexture tempTexture1;
-        private RenderTexture tempTexture2;
-
         private Camera mainCamera;
 
-        private const int iterations = 7;
+        private const int iterations = 2;
 
         private void Awake()
         {
             mainCamera = GetComponent<Camera>();
 
-            tempTexture1 = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.ARGB32);
-            tempTexture2 = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.ARGB32);
+            renderTextures = new RenderTexture[portals.Length];
+
+            for (var i = 0; i < portals.Length; i++)
+            {
+                renderTextures[i] = new RenderTexture(Screen.width, Screen.height, 24, RenderTextureFormat.ARGB32);
+            }
         }
 
         private void Start()
         {
-            portals[0].SetTexture(tempTexture1);
-            portals[1].SetTexture(tempTexture2);
+            for (var i = 0; i < portals.Length; i++)
+            {
+                portals[i].SetTexture(renderTextures[i]);
+            }
         }
 
         private void OnPreRender()
         {
-            if (!portals[0].IsPlaced() || !portals[1].IsPlaced())
+            for (var portalPairIndex = 0; portalPairIndex < portals.Length-1; portalPairIndex += 2)
             {
-                return;
-            }
-
-            if (portals[0].IsRendererVisible())
-            {
-                portalCamera.targetTexture = tempTexture1;
-                for (int i = iterations - 1; i >= 0; --i)
+                if (!portals[portalPairIndex].IsPlaced() || !portals[portalPairIndex+1].IsPlaced())
                 {
-                    RenderCamera(portals[0], portals[1], i);
+                    return;
                 }
-            }
 
-            if(portals[1].IsRendererVisible())
-            {
-                portalCamera.targetTexture = tempTexture2;
-                for (int i = iterations - 1; i >= 0; --i)
+                if (portals[portalPairIndex].IsRendererVisible())
                 {
-                    RenderCamera(portals[1], portals[0], i);
+                    portalCamera.targetTexture = renderTextures[portalPairIndex];
+                    for (var i = iterations - 1; i >= 0; --i)
+                    {
+                        RenderCamera(portals[0], portals[1], i);
+                    }
+                }
+
+                if (portals[portalPairIndex+1].IsRendererVisible())
+                {
+                    portalCamera.targetTexture = renderTextures[portalPairIndex+1];
+                    for (var i = iterations - 1; i >= 0; --i)
+                    {
+                        RenderCamera(portals[portalPairIndex+1], portals[portalPairIndex], i);
+                    }
                 }
             }
         }
