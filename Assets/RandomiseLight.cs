d0608f1ee2c3cff242b55727e52fa2b1757b7fd7
Primoz Pecar <primoz.pecar@outfit7.com>
2020-02-01 19:13:40 +0100

    rigging, player working hands colliders

---
+++ Assets/RandomiseLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomiseLight : MonoBehaviour
+{
+    private Light light_;
+    private float start_time_;
+    void Start()
+    {
+        light_ = GetComponent<Light>();
+        start_time_ = 0.0f;
+    }
+    void Update()
+    {
+        light_.color = Color.Lerp(light_.color, new Color(
+      Random.Range(0f, 1f),
+      Random.Range(0f, 1f),
+      Random.Range(0f, 1f)), Time.deltaTime);
+    }
+}
---
+++ Assets/RoomAngleController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomAngleController : MonoBehaviour
+{
+    public GameObject parentRoom;
+
+    void Update()
+    {
+        parentRoom.transform.rotation = transform.rotation;    
+    }
+}
