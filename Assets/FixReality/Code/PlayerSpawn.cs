7750be350d07c114500f549acd02ff7ddecdd2a8
moses <amadej.cekuta@gmail.com>
2020-02-01 19:41:57 +0100

    player se spawna ce pade s potke

---
+++ Assets/FixReality/Code/PlayerSpawn.cs
@@ -0,0 +1,25 @@
+﻿using BezierSolution;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    public BezierSpline Spline;
+    public GameObject Player;
+    public float fallDistance = 20f;
+
+    private Vector3 playerPosition;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPosition = Player.transform.position;
+
+        if (Player.transform.position.y < Spline.transform.position.y - fallDistance)
+        {
+            Player.transform.position = Spline.transform.position + fallDistance * Vector3.up;
+        }
+    }
+}
