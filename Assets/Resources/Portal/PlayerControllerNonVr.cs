a29820616bb892b8eaf2c911e322f54833f9eaed
matevz.peternel <matevz.peternel@yahoo.com>
2020-02-02 13:11:09 +0100

    Novr support

--- Assets/Resources/Portal/PlayerControllerNonVr.cs
+++ Assets/Resources/Portal/PlayerControllerNonVr.cs
@@ -8,9 +8,22 @@ namespace Assets.Resources.Portal
 
         public bool InsidePortal;
         public Portal EnteredPortal;
-
+        bool active = false;
         private void Update()
         {
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                active = true;
+                var list=    gameObject.GetComponentsInChildren<Collider>();
+                foreach(var col in list)
+                {
+                    col.enabled = false;
+                }
+            }
+            if(!active)
+            {
+                return;
+            }
             var movement = transform.right * Input.GetAxisRaw("Horizontal") +
                            transform.forward * Input.GetAxisRaw("Vertical");
             movement = movement.normalized * MovementSpeed * Time.deltaTime;
