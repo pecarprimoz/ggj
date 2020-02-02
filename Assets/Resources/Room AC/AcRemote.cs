2912f5532ff8e73112e85c268ee9248fd3deecf2
dmajster <domen.rostohar98@gmail.com>
2020-02-02 17:13:09 +0100

    NEW: Added VR button to control the AC

---
+++ Assets/Resources/Room AC/AcRemote.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Resources.Room_AC
+{
+    public class AcRemote : MonoBehaviour
+    {
+        public AcUnit Ac;
+
+        public float DebounceTime = 1.0f;
+        private float _lastUseTime;
+
+        public void OnQuitButtonPressed()
+        {
+            var currentTime = Time.time;
+
+            if (currentTime - _lastUseTime > DebounceTime)
+            {
+                Debug.Log("Quit button pressed");
+                _lastUseTime = currentTime;
+                Ac.Status = Ac.Status == AcUnitStatus.Off ? AcUnitStatus.Opening : AcUnitStatus.Closing;
+            }
+        }
+    }
+}
---
+++ Assets/Resources/Room AC/AcUnit.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using UnityEngine;
+
+namespace Assets.Resources.Room_AC
+{
+    public enum AcUnitStatus
+    {
+        Opening,
+        Operating,
+        Closing,
+        Off
+    }
+
+    public class AcUnit : MonoBehaviour
+    {
+        public AcUnitStatus Status;
+
+        public GameObject FlapOne;
+        public Vector3 FlapOneClosedRotation;
+        public Vector3 FlapOneOperatingInitialRotation;
+        public Vector3 FlapOneOperatingFinalRotation;
+
+        public GameObject FlapTwo;
+        public Vector3 FlapTwoClosedRotation;
+        public Vector3 FlapTwoOperatingInitialRotation;
+        public Vector3 FlapTwoOperatingFinalRotation;
+
+        public bool FlapsGoingDown;
+        public float FlapsOperatingMovementSpeed;
+        public float FlapsOperatingOpenPercentage;
+
+        public float FlapsClosingSpeed;
+
+
+
+        private void Update()
+        {
+            switch (Status)
+            {
+                case AcUnitStatus.Opening:
+                    Opening();
+                    break;
+                case AcUnitStatus.Operating:
+                    Operating();
+                    break;
+                case AcUnitStatus.Closing:
+                    Closing();
+                    break;
+                case AcUnitStatus.Off:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private void Opening()
+        {
+            if (FlapOne.transform.eulerAngles.x < FlapOneOperatingInitialRotation.x)
+            {
+                FlapOne.transform.eulerAngles += transform.right * FlapsClosingSpeed * Time.deltaTime;
+            }
+
+            if (FlapTwo.transform.eulerAngles.x < FlapTwoOperatingInitialRotation.x)
+            {
+                FlapTwo.transform.eulerAngles += transform.right * FlapsClosingSpeed * Time.deltaTime;
+            }
+
+            if (FlapOne.transform.eulerAngles.x >= FlapOneOperatingInitialRotation.x && FlapTwo.transform.eulerAngles.x >= FlapTwoOperatingInitialRotation.x)
+            {
+                Status = AcUnitStatus.Operating;
+            }
+        }
+
+        private void Operating()
+        {
+            FlapsOperatingOpenPercentage += (FlapsGoingDown ? FlapsOperatingMovementSpeed : -FlapsOperatingMovementSpeed) * Time.deltaTime;
+
+            if (FlapsOperatingOpenPercentage <= 0.0f)
+            {
+                FlapsGoingDown = true;
+            }
+            else if (FlapsOperatingOpenPercentage >= 1.0f)
+            {
+                FlapsGoingDown = false;
+            }
+
+            FlapOne.transform.rotation = Quaternion.Lerp(Quaternion.Euler(FlapOneOperatingInitialRotation), Quaternion.Euler(FlapOneOperatingFinalRotation), FlapsOperatingOpenPercentage);
+            FlapTwo.transform.rotation = Quaternion.Lerp(Quaternion.Euler(FlapTwoOperatingInitialRotation), Quaternion.Euler(FlapTwoOperatingFinalRotation), FlapsOperatingOpenPercentage);
+        }
+
+        private void Closing()
+        {
+            if (FlapOne.transform.eulerAngles.x > 1.0f)
+            {
+                FlapOne.transform.eulerAngles -= transform.right * FlapsClosingSpeed * Time.deltaTime;
+            }
+
+            Debug.Log($"Flap one: {FlapOne.transform.eulerAngles.x}");
+
+            if (FlapTwo.transform.eulerAngles.x > 1.0f)
+            {
+                FlapTwo.transform.eulerAngles -= transform.right * FlapsClosingSpeed * Time.deltaTime;
+            }
+
+            Debug.Log($"Flap two: {FlapTwo.transform.eulerAngles.x}");
+
+            if (FlapOne.transform.eulerAngles.x <= 1.0f && FlapTwo.transform.eulerAngles.x <= 1.0f)
+            {
+                Status = AcUnitStatus.Off;
+            }
+        }
+    }
+}
